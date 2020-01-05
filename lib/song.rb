e0e4ae79c111a3ae0961b4c1e10d6826455faec6
Taryn Johnson <taryn.johnson919@gmail.com>
2020-01-05 01:40:25 +0000

    Automatically backed up by Learn

---
+++ lib/song.rb
@@ -0,0 +1,19 @@
+class Song 
+  
+  @@count = 0 
+  
+attr_accessor :name, :artist, :genre 
+
+def initialize (name, artist, genre)
+  @name = name 
+  @artist = artist 
+  @genre = genre 
+end 
+  
+  
+  
+  
+  
+  
+  
+end 

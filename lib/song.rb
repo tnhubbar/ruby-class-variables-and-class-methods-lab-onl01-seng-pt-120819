2c3561ab80d091e9157431c86870e322986f4a6a
Taryn Johnson <taryn.johnson919@gmail.com>
2020-01-05 01:59:53 +0000

    Automatically backed up by Learn

--- lib/song.rb
+++ lib/song.rb
@@ -35,14 +35,7 @@ end
   end 
   
   def self.artist_count
-    @@artists.each do |artist|
-      if artist_count[artist]
-        artist_count[artist] += 1 
-      else
-        artist_count[artist] = 1
-      end
-    end
-    artist_count
+  
   end 
   
 end 
